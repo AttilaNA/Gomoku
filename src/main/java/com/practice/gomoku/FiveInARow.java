bbac9d76b229ae263a594e23f43b47d6b67e13ba
Attila Nagy <nagyattila86.hu@gmail.com>
2023-05-10 18:19:51 +0200

    Ask for user input and return the coordinates of a valid move on board
    Coordinates are specified as a letter and a number, for example, A2 is the first row of the second column, and C1 is the third row of the first column.
    
    The function returns an array of integers containing the row and column number corresponding to the input.
    
    The function returns an array of integers containing the row and column number corresponding to the input.
    
    The integers indicate a valid (empty) position on the board.
    
    If the user provides coordinates that are outside of a 26x26 sized board, keep asking.
    
    If the user provides coordinates for a place that is taken, keep asking.
    
    If the user provides coordinates for a place that is taken, keep asking.
    
    If the user provides input that does not follow the format of coordinates, keep asking.

--- src/main/java/com/practice/gomoku/FiveInARow.java
+++ src/main/java/com/practice/gomoku/FiveInARow.java
@@ -1,9 +1,20 @@
 package com.practice.gomoku;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FiveInARow {
 
     public static void main(String[] args) {
         Game game = new Game(11, 11);
+        int[][] testBoard = game.getBoard();
+        testBoard[0][0] = 1;
+        game.setBoard(testBoard);
+        int[] move = {26, 26};
+        while (Arrays.equals(move, new int[] {26, 26})) {
+            move = game.getMove(1);
+        }
         game.enableAi(1);
         game.enableAi(2);
         game.play(5);
--- src/main/java/com/practice/gomoku/Game.java
+++ src/main/java/com/practice/gomoku/Game.java
@@ -1,10 +1,17 @@
 package com.practice.gomoku;
 
+import java.util.Scanner;
+import java.util.regex.*;
+
 public class Game implements GameInterface {
 
     private int[][] board;
 
     public Game(int nRows, int nCols) {
+        nRows = (nRows > 26) ? 26 : nRows;
+        nRows = (nRows < 5) ? 5 : nRows;
+        nCols = (nCols > 26) ? 26 : nCols;
+        nCols = (nCols < 5) ? 5 : nRows;
         board = new int[nRows][nCols];
         for(int i = 0; i < nRows; i++){
             for(int j = 0; j < nCols; j++) {
@@ -22,7 +29,29 @@ public class Game implements GameInterface {
     }
 
     public int[] getMove(int player) {
-        return null;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Next move: ");
+        String userInput = sc.next();
+        Pattern pattern = Pattern.compile("^[a-zA-Z]([1-9]|1[0-9]|2[0-6])$");
+        Matcher matcher = pattern.matcher(userInput);
+        if(matcher.find()){
+            int[] possibleMove = getArrayOfTwoIntegersFromUserInput(userInput.toLowerCase());
+            if(board[possibleMove[0]][possibleMove[1]] == 0){
+                return possibleMove;
+            }
+        }
+        return new int[] {26, 26};
+    }
+
+    private int[] getArrayOfTwoIntegersFromUserInput(String userInput) {
+        String alphabet = "";
+        for(char c='a'; c <= 'z'; c++){
+            alphabet += c;
+        }
+        int[] coordinate = new int[2];
+        coordinate[0] = alphabet.indexOf(userInput.toCharArray()[0]);
+        coordinate[1] = Integer.parseInt(userInput.substring(1)) - 1;
+        return coordinate;
     }
 
     public int[] getAiMove(int player) {
